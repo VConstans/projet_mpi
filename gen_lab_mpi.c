b8001544dba4e0095a6d07f7db123c96597da123
Victor <victor.constans@etu.unistra.fr>
2018-05-06 12:55:34 +0200

    Splite work through MPI processes

--- gen_lab_mpi.c
+++ gen_lab_mpi.c
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 
 #include <mpi.h>
+#include <string.h>
 
 /* à commenter pour désactiver l'affichage */
 #define AFFICHE
@@ -60,6 +61,31 @@ int estconstructible( size_t N, size_t M, int l[N][M], int i, int j)
 }
 
 
+void ecriture(int N, int M, int l[N][M],char* file)
+{
+		int f = open( file, O_WRONLY|O_CREAT, 0644 );
+		int x = N;
+		if(write( f, &x, sizeof(int) ) == -1)
+		{
+			perror("Erreur write");
+			exit(EXIT_FAILURE);
+		}
+		x = M;
+		if(write( f, &x, sizeof(int) ) == -1)
+		{
+			perror("Erreur write");
+			exit(EXIT_FAILURE);
+		}
+		if(write( f, l, N*M*sizeof(int) ) == -1)
+		{
+			perror("Erreur write");
+			exit(EXIT_FAILURE);
+		}
+		close( f );
+}
+
+
+
 int main(int argc, char* argv[argc+1])
 {
 	if(MPI_Init(&argc,&argv))
@@ -77,13 +103,13 @@ int main(int argc, char* argv[argc+1])
 
 	//TODO ceci doit etre seulement fait par rank == 0 ?
 
-	int i = 0, j = 0, nbilots = NBILOTS, nbcons;
+	int i = 0, j = 0, nbilots = NBILOTS/size, nbcons;
 #ifdef AFFICHE
 	int ref=REFRESH;
 #endif /* AFFICHE */
 
 	if( argc > 1 )
-		nbilots = strtoull(argv[1], 0, 0);
+		nbilots = strtoull(argv[1], 0, 0)/size;
 
 	/* taille du labyrinthe : */
 	/* hauteur : */
@@ -98,102 +124,27 @@ int main(int argc, char* argv[argc+1])
 
 	srand( time(0) );
 
-	/* initialise l : murs autour, vide a l'interieur */
-	for( i=0 ; i<N ; i++ )
-		for( j=0 ; j<M ; j++ )
-			if( i==0 || i==N-1 ||j==0 || j==M-1 )
-			{
-				l[i][j] = 0; /* mur */
-			}
-			else
-				l[i][j] = 1; /* vide */
-
-	/* place <nbilots> ilots aleatoirement a l'interieur du laby */
-	for( ; nbilots ; nbilots-- )
-	{
-		i = rand()%(N-4) + 2;
-		j = rand()%(M-4) + 2;
-		l[i][j] = 0;
-	}
-
-#ifdef AFFICHE
-	initgraph(M*(CARRE+INTER), N*(CARRE+INTER));
-	for( int i=0 ; i<N ; i++ )
-		for( int j=0 ; j<M ; j++ )
-			if( l[i][j]==0 )
-				affichecarre(i,j);
-	refresh();
-#endif /* AFFICHE */
-
-
-
-	/* initialise les cases constructibles */
-	nbcons = 0;
-	for( int i=1 ; i<N-1 ; i++ )
-		for( int j=1 ; j<M-1 ; j++ )
-			if( estconstructible( N, M, l, i, j ) )
-			{
-				l[i][j] = -1;
-				nbcons++;
-			}
-	/* supprime quelques cases constructibles sur les bords */
-	for( int i=1 ; i<N-1 ; i++ )
-	{
-		if( l[i][1] == -1 && (rand()%PROBPASCONS) && nbcons>(CONSMIN*2) )
-		{
-			l[i][1] = 1;
-			nbcons--;
-		}
-		if( l[i][M-2] == -1 && (rand()%PROBPASCONS) && nbcons>(CONSMIN*2) )
-		{
-			l[i][M-2] = 1;
-			nbcons--;
-		}
-	}
-  	for( int j=1 ; j<M-1 ; j++ )
-  	{
-		if( l[1][j] == -1 && (rand()%PROBPASCONS) && nbcons>CONSMIN )
-		{
-			l[1][j] = 1;
-			nbcons--;
-		}
-		if( l[N-2][j] == -1 && (rand()%PROBPASCONS) && nbcons>CONSMIN )
-		{
-			l[N-2][j] = 1;
-			nbcons--;
-		}
-	}
-
-	printf("Avant\n");
-
-
 
 	int taille_chunk = N / size;
 	int taille_dernier_chunk = N - (taille_chunk * (size - 1));
 
-	/*
-	MPI_Datatype chunk_type;
-	MPI_Datatype dernier_chunk_type;
-	if(MPI_Type_vector(N,taille_chunk,M,MPI_INT,&chunk_type) != MPI_SUCCESS)
-	{
-		errno("Erreur MPI_type_vector");
-		MPI_Finalize();
-		exit(EXIT_FAILURE);
-	}
-
-	if(MPI_Type_vector(N,taille_dernier_chunk,M,MPI_INT,&dernier_chunk_type) != MPI_SUCCESS)
-	{
-		errno("Erreur MPI_type_vector");
-		MPI_Finalize();
-		exit(EXIT_FAILURE);
-	}*/
-
 	if(rank == 0)
 	{
+
+		/* initialise l : murs autour, vide a l'interieur */
+		for( i=0 ; i<N ; i++ )
+			for( j=0 ; j<M ; j++ )
+				if( i==0 || i==N-1 ||j==0 || j==M-1 )
+				{
+					l[i][j] = 0; /* mur */
+				}
+				else
+					l[i][j] = 1; /* vide */
+
 		int i;
-		for(i=0;i<size-1;i++)
+		for(i=1;i<size-1;i++)
 		{
-			if(MPI_Send(&l[i*taille_chunk],taille_chunk,MPI_INT,i,i,MPI_COMM_WORLD) != MPI_SUCCESS)
+			if(MPI_Send(&l[i*taille_chunk],taille_chunk*M,MPI_INT,i,i,MPI_COMM_WORLD) != MPI_SUCCESS)
 			{
 				perror("Erreur ssend");
 				MPI_Finalize();
@@ -201,7 +152,7 @@ int main(int argc, char* argv[argc+1])
 			}
 		}
 
-		if(MPI_Send(&l[i*taille_chunk],taille_dernier_chunk,MPI_INT,i,i,MPI_COMM_WORLD) != MPI_SUCCESS)
+		if(MPI_Send(&l[i*taille_chunk],taille_dernier_chunk*M,MPI_INT,i,i,MPI_COMM_WORLD) != MPI_SUCCESS)
 		{
 			perror("Erreur ssend");
 			MPI_Finalize();
@@ -222,33 +173,133 @@ int main(int argc, char* argv[argc+1])
 		taille_chunk_courant = taille_chunk;
 	}
 
-	int* chunk = (int*) malloc (taille_chunk_courant * sizeof(int));
+        int (*chunk)[M] = malloc(sizeof(int[taille_chunk_courant][M]));
 	MPI_Status status;
 
-	if(MPI_Recv(chunk,taille_chunk_courant,MPI_INT,0,rank,MPI_COMM_WORLD,&status) != MPI_SUCCESS)
+	if(rank == 0)
 	{
-		perror("Erreur recv");
+		memcpy(chunk,l,taille_chunk_courant*M*sizeof(int));	
+	}
+	else
+	{
+		if(MPI_Recv(chunk,taille_chunk_courant*M,MPI_INT,0,rank,MPI_COMM_WORLD,&status) != MPI_SUCCESS)
+		{
+			perror("Erreur recv");
+			MPI_Finalize();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	printf("Recu\n");
+
+	char file[6];
+	sprintf(file,"%d.out",rank);
+
+	ecriture(taille_chunk_courant,M,chunk,file);
+
+
+
+	/* place <nbilots> ilots aleatoirement a l'interieur du laby */
+	for( ; nbilots ; nbilots-- )
+	{
+		i = rand()%(taille_chunk_courant-4) + 2;
+		j = rand()%(M-4) + 2;
+		chunk[i][j] = 0;
+	}
+
+
+	//TODO affichage que par le 0
+
+#ifdef AFFICHE
+	initgraph(M*(CARRE+INTER), N*(CARRE+INTER));
+	for( int i=0 ; i<taille_chunk_courant ; i++ )
+		for( int j=0 ; j<M ; j++ )
+			if( chunk[i][j]==0 )
+				affichecarre(i,j);
+	refresh();
+#endif /* AFFICHE */
+
+
+
+	/* initialise les cases constructibles */
+	nbcons = 0;
+	for( int i=1 ; i<taille_chunk_courant-1 ; i++ )
+		for( int j=1 ; j<M-1 ; j++ )
+			if( estconstructible( taille_chunk_courant, M, chunk, i, j ) )
+			{
+				chunk[i][j] = -1;
+				nbcons++;
+			}
+	/* supprime quelques cases constructibles sur les bords */
+	for( int i=1 ; i<taille_chunk_courant-1 ; i++ )
+	{
+		if( chunk[i][1] == -1 && (rand()%PROBPASCONS) && nbcons>(CONSMIN*2) )
+		{
+			chunk[i][1] = 1;
+			nbcons--;
+		}
+		if( chunk[i][M-2] == -1 && (rand()%PROBPASCONS) && nbcons>(CONSMIN*2) )
+		{
+			chunk[i][M-2] = 1;
+			nbcons--;
+		}
+	}
+  	for( int j=1 ; j<M-1 ; j++ )
+  	{
+		if( chunk[1][j] == -1 && (rand()%PROBPASCONS) && nbcons>CONSMIN )
+		{
+			chunk[1][j] = 1;
+			nbcons--;
+		}
+		if( chunk[taille_chunk_courant-2][j] == -1 && (rand()%PROBPASCONS) && nbcons>CONSMIN )
+		{
+			chunk[taille_chunk_courant-2][j] = 1;
+			nbcons--;
+		}
+	}
+
+	printf("Avant\n");
+
+
+
+
+	/*
+	MPI_Datatype chunk_type;
+	MPI_Datatype dernier_chunk_type;
+	if(MPI_Type_vector(N,taille_chunk,M,MPI_INT,&chunk_type) != MPI_SUCCESS)
+	{
+		errno("Erreur MPI_type_vector");
 		MPI_Finalize();
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Recu\n");
+	if(MPI_Type_vector(N,taille_dernier_chunk,M,MPI_INT,&dernier_chunk_type) != MPI_SUCCESS)
+	{
+		errno("Erreur MPI_type_vector");
+		MPI_Finalize();
+		exit(EXIT_FAILURE);
+	}*/
+
 	
 	/* boucle principale de génération */
+
+	printf("cons %d\n",nbcons);
+
+
 	while( nbcons )
 	{
 		int r = 1 + rand() % nbcons;
-		for( i=1 ; i<N-1 ; i++ )
+		for( i=1 ; i<taille_chunk_courant-1 ; i++ )
 		{
 			for( j=1 ; j<M-1 ; j++ )
-				if( l[i][j] == -1 )
+				if( chunk[i][j] == -1 )
 					if( ! --r )
 						break;
 			if( ! r )
 				break;
 		}
 		/* on construit en (i,j) */
-		l[i][j] = 0;
+		chunk[i][j] = 0;
 
 #ifdef AFFICHE
 		affichecarre(i,j);
@@ -263,19 +314,19 @@ int main(int argc, char* argv[argc+1])
 		/* met a jour les 8 voisins */
 		for( int ii=i-1 ; ii<=i+1 ; ++ii )
 			for( int jj=j-1 ; jj<=j+1 ; ++jj )
-				if( l[ii][jj]==1 && estconstructible(N, M, l, ii,jj) )
+				if( chunk[ii][jj]==1 && estconstructible(taille_chunk_courant, M, chunk, ii,jj) )
 				{
 					nbcons ++;
-					l[ii][jj] = -1;
+					chunk[ii][jj] = -1;
 				}
-				else if( l[ii][jj]==-1 && ! estconstructible(N, M, l, ii,jj) )
+				else if( chunk[ii][jj]==-1 && ! estconstructible(taille_chunk_courant, M, chunk, ii,jj) )
 				{
 					nbcons --;
-					l[ii][jj] = 1;
+					chunk[ii][jj] = 1;
 				}
 	}	/* fin while */
 
-	if(MPI_Send(chunk,taille_chunk_courant,MPI_INT,0,rank,MPI_COMM_WORLD) != MPI_SUCCESS)
+	if(MPI_Send(chunk,taille_chunk_courant*M,MPI_INT,0,rank,MPI_COMM_WORLD) != MPI_SUCCESS)
 	{
 		perror("Erreur ssend");
 		MPI_Finalize();
@@ -289,7 +340,7 @@ int main(int argc, char* argv[argc+1])
 	{
 		for(i=0;i<size-1;i++)
 		{
-			if(MPI_Recv(&l[i*taille_chunk],taille_chunk,MPI_INT,i,i,MPI_COMM_WORLD,&status) != MPI_SUCCESS)
+			if(MPI_Recv(&l[i*taille_chunk],taille_chunk*M,MPI_INT,i,i,MPI_COMM_WORLD,&status) != MPI_SUCCESS)
 			{
 				perror("Erreur recv");
 				MPI_Finalize();
@@ -297,7 +348,7 @@ int main(int argc, char* argv[argc+1])
 			}
 		}
 
-		if(MPI_Recv(&l[i*taille_chunk],taille_dernier_chunk,MPI_INT,i,i,MPI_COMM_WORLD,&status) != MPI_SUCCESS)
+		if(MPI_Recv(&l[i*taille_chunk],taille_dernier_chunk*M,MPI_INT,i,i,MPI_COMM_WORLD,&status) != MPI_SUCCESS)
 		{
 			perror("Erreur recv");
 			MPI_Finalize();
